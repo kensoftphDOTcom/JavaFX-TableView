cfe06568bf1942b87470e1fb2052fab655c92b83
Kensoft PH <kensoftph.com@gmail.com>
2023-05-27 15:29:21 +0800

    modifying the code creating the table view without using an FXML file

--- src/main/java/com/kensoftph/tableview/Main.java
+++ src/main/java/com/kensoftph/tableview/Main.java
@@ -3,6 +3,8 @@ package com.kensoftph.tableview;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +12,24 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("table-view.fxml"));
-        Scene scene = new Scene(loader.load());
-        stage.setTitle("JavaFX TableView");
+        //FXMLLoader loader = new FXMLLoader(getClass().getResource("table-view.fxml"));
+        //Scene scene = new Scene(loader.load());
+        //stage.setTitle("JavaFX TableView");
+        //stage.setScene(scene);
+
+
+        // these lines of code here will be creating the tableview without using an FXML file
+        TableView<Person> tableView = new TableView<>(Person.getInitialList());
+        tableView.getColumns().addAll(Person.getFistNameCol(), Person.getLastNameCol(), Person.getOriginCol());
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        StackPane layout = new StackPane(tableView);
+
+        Scene scene = new Scene(layout, 400, 400);
         stage.setScene(scene);
+        stage.setTitle("TableView in JavaFX");
+        // end
+
         stage.show();
     }
 
